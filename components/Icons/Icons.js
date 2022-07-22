d541ded95b0c28b49680084b4553e6b43706901f
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-22 19:20:16 -0300

    add object icons

---
+++ components/Icons/Icons.js
@@ -0,0 +1,100 @@
+import { AiFillHome, AiFillCompass, AiOutlineYoutube, AiOutlineMenu,
+AiOutlineDownload, AiOutlineLike, AiOutlineDislike, AiOutlineUser,
+AiOutlinePlaySquare, AiOutlineClockCircle, AiOutlineSetting, AiOutlineDollarCircle
+
+} from 'react-icons/ai';
+
+import { GiHamburgerMenu, GiAerialSignal } from 'react-icons/gi';
+import { VscMenu } from 'react-icons/vsc';
+import { SiYoutubemusic, SiYoutubegaming } from 'react-icons/si';
+import {  RiVideoAddLine, RiShieldUserLine } from 'react-icons/ri';
+import { GrHistory } from 'react-icons/gr';
+import { FiHelpCircle } from 'react-icons/fi';
+import { TbUserCircle } from 'react-icons/tb';
+import { FaRegKeyboard } from 'react-icons/fa';
+import { IoLanguageOutline } from 'react-icons/io';
+import { MdLocalMovies, MdOutlineKeyboardArrowDown, MdOutlineSwitchAccount, MdOutlineMonetizationOn } from 'react-icons/md';
+import { BsCollectionPlay, BsPlayBtnFill, BsFlag, BsExclamationSquare, BsBoxArrowInRight,BsMoon,
+    BsTrophy, BsLightbulb, BsPlayCircle,BsBell, BsSearch, BsMicFill, BsGlobe } from 'react-icons/bs';
+
+const homeIcon = <AiFillHome />
+const navIcon = <AiOutlineMenu />
+const exploreIcon = <AiFillCompass />
+const originalsIcon = <AiFillCompass />
+const musicIcon = <SiYoutubemusic />
+const videosIcon = <AiOutlinePlaySquare />
+const subscriptionsIcon = <BsCollectionPlay />
+const libraryIcon = <BsPlayBtnFill />
+const likedIcon = <AiOutlineLike />
+const downloadIcon = <AiOutlineDownload />
+const clockIcon = <AiOutlineClockCircle />
+const historyIcon = <GrHistory />
+const arrowDownIcon = <MdOutlineKeyboardArrowDown />
+const settingsIcon = <AiOutlineSetting />
+const helpIcon = <FiHelpCircle />
+const flagIcon = <BsFlag />
+const exclamationIcon = <BsExclamationSquare />
+const moviesIcon = <MdLocalMovies />
+const gamingIcon = <SiYoutubegaming />
+const trophyIcon = <BsTrophy />
+const liveIcon = <GiAerialSignal />
+const lightIcon = <BsLightbulb />
+const shortIcon = <BsPlayCircle />
+
+const addVideoIcon = <RiVideoAddLine />
+const bellIcon = <BsBell />
+const searchIcon = <BsSearch />
+const micIcon = <BsMicFill />
+
+
+const userIcon = <AiOutlineUser />
+// const youtubeStudioIcon= <AiOutlineUser />
+const accountIcon= <MdOutlineSwitchAccount />
+const signOutIcon= <BsBoxArrowInRight />
+
+
+const membershipsIcon= <AiOutlineDollarCircle />
+const yourDataIcon= <TbUserCircle />
+const darkIcon= <BsMoon />
+const languageIcon= <IoLanguageOutline />
+const restrictedIcon= <RiShieldUserLine />
+const locationIcon= <BsGlobe />
+const keyboardIcon= <FaRegKeyboard />
+
+
+ const icons = {
+
+    homeIcon,
+    navIcon,
+    settingsIcon,
+    helpIcon,
+    flagIcon,
+    exclamationIcon,
+    moviesIcon,
+    gamingIcon,
+    trophyIcon,
+    liveIcon,
+    lightIcon,
+    exploreIcon,
+    originalsIcon,
+    musicIcon,
+    videosIcon,
+    subscriptionsIcon,
+    libraryIcon,
+    likedIcon,
+    downloadIcon,
+    clockIcon,
+    historyIcon,
+    arrowDownIcon,
+    shortIcon,
+    addVideoIcon,
+    bellIcon,
+    searchIcon,
+    micIcon,
+
+
+}
+
+export default icons
+
+
