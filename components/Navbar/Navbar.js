a0b47b3b9bd720ab1b6cffc97397473f8ea307db
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-29 20:29:31 -0300

    add container card with your style

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,50 +2,66 @@ import React from 'react'
 import Li from '../Li/Li'
 import icons from '../Icons/Icons'
 import styles from './navbar.module.css'
+import Card from '../CardVideo/Card'
+import { subscriptions } from '../../pages/api/apiMovies'
+import LiSubs from '../Li/LiSubs'
 
-const Navbar = () => {
+const Navbar = ({children}) => {
   return (
 
-        // <div className={styles.navbar}>
+<div className={styles.container_navbar}>
+        <div className={styles.navbar}>
                 <ul className={styles.list_container}>
 
-                {/* <li className={styles.iconNavbar}>{icons.navIcon}
-                
-                <img src="https://res.cloudinary.com/dj1pp4ivb/image/upload/v1658366305/fondoPremium_lug99w.png" width="120"/>
-                </li> */}
+                <div className={styles.container_icons}>
+                        <Li text="Home" icon={icons.homeIcon}/>
+                        <Li text="Explore" icon={icons.exploreIcon}/>
+                        <Li text="Shorts" icon={icons.shortIcon}/>
+                        <Li text="Subscriptions" icon={icons.subscriptionsIcon}/>
+                        <Li text="Originals" icon={icons.originalsIcon}/>
+                        <Li text="YouTube Music" icon={icons.musicIcon}/>
+                        <hr className={styles.section_separator}/>
+
+                        <Li text="Library" icon={icons.libraryIcon}/>
+                        <Li text="History" icon={icons.historyIcon}/>
+                        <Li text="Your Videos" icon={icons.videosIcon}/>
+                        <Li text="Watch later" icon={icons.clockIcon}/>
+                        <Li text="Download" icon={icons.downloadIcon}/>
+                        <Li text="Liked videos" icon={icons.likedIcon}/>
+                        <Li text="Show more" icon={icons.arrowDownIcon}/>
+
+                        <hr className={styles.section_separator}/>
+                        <Li text="SUBSCRIPTIONS" type={'text'}/>
+                                {subscriptions.map(subs => (
+                                <LiSubs text={subs.name} img={subs.img}/>
+                                ))}
                 
 
-            <div className={styles.container_icons}>
-                    <Li text="Home" icon={icons.homeIcon}/>
-                    <Li text="Explore" icon={icons.exploreIcon}/>
-                    <Li text="Shorts" icon={icons.shortIcon}/>
-                    <Li text="Subscriptions" icon={icons.subscriptionsIcon}/>
-                    <Li text="Originals" icon={icons.originalsIcon}/>
-                    <Li text="YouTube Music" icon={icons.musicIcon}/>
-                    <Li text="Library" icon={icons.libraryIcon}/>
-                    <Li text="History" icon={icons.historyIcon}/>
-                    <Li text="Your Videos" icon={icons.videosIcon}/>
-                    <Li text="Watch later" icon={icons.clockIcon}/>
-                    <Li text="Download" icon={icons.downloadIcon}/>
-                    <Li text="Liked videos" icon={icons.likedIcon}/>
-                    <Li text="Show more" icon={icons.arrowDownIcon}/>
-                    <Li text="Show more" icon={icons.homeIcon}/>
-                    <Li text="Show more" icon={icons.homeIcon}/>
-                    <Li text="Show more" icon={icons.homeIcon}/>
-                    <Li text="Show more" icon={icons.homeIcon}/>
-                    <Li text="Show more" icon={icons.homeIcon}/>
-                    <Li text="Show more" icon={icons.homeIcon}/>
-                    <Li text="Show more" icon={icons.homeIcon}/>
-                    <Li text="Show more" icon={icons.homeIcon}/>
-                    <Li text="Show more" icon={icons.homeIcon}/>
-                    <Li text="Show more" icon={icons.homeIcon}/>
-            </div>
+                        <hr className={styles.section_separator}/>
+                        <Li text="EXPLORE" type={'text'}/>
+                        <Li text="Movies" icon={icons.moviesIcon}/>
+                        <Li text="Gaming" icon={icons.gamingIcon}/>
+                        <Li text="Live" icon={icons.liveIcon}/>
+                        <Li text="Learning" icon={icons.lightIcon}/>
+                        <Li text="Sports" icon={icons.trophyIcon}/>
+                        <hr className={styles.section_separator}/>
+                        <Li text="Settings" icon={icons.settingsIcon}/>
+                        <Li text="Report history" icon={icons.flagIcon}/>
+                        <Li text="Help" icon={icons.helpIcon}/>
+                        <Li text="Send feedback" icon={icons.exclamationIcon}/>
+                        <hr className={styles.section_separator}/>
 
+                </div>
                 </ul>
+        </div>
 
-          // </div>
+        <div className={styles.container_card}>
 
+                {children}
+                
+        </div>
 
+</div>
 
 
   )
