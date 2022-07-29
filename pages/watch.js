0ac7f7946adad4540c1b856b9f94285a60782774
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-28 23:31:59 -0300

    add watch pages

---
+++ pages/watch.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Homelayout from '../layout/Homelayout'
+import Header from '../components/Header/Header'
+import Navbar from '../components/Navbar/Navbar'
+
+const watch = () => {
+  return (
+    <Homelayout>
+          <Header />
+
+          <Navbar>
+
+
+          </Navbar>
+
+    </Homelayout>
+  )
+}
+
+export default watch
