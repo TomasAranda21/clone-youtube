22f94cd15e646f2680e4745db14d67d83046985e
Tomas Aranda <tomasaranda@cybertiks.com>
2022-08-22 21:02:19 -0300

    refactor navigation

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -5,8 +5,10 @@ import styles from './navbar.module.css'
 import Card from '../CardVideo/Card'
 import { subscriptions } from '../../pages/api/apiMovies'
 import LiSubs from '../Li/LiSubs'
+import { navExplore, navigation, navigationTwo, navSettings } from '../../routes'
 
 const Navbar = ({children}) => {
+
   return (
 
 <div className={styles.container_navbar}>
@@ -14,53 +16,53 @@ const Navbar = ({children}) => {
                 <ul className={styles.list_container}>
 
                 <div className={styles.container_icons}>
-                        <Li text="Home" icon={icons.homeIcon}/>
-                        <Li text="Explore" icon={icons.exploreIcon}/>
-                        <Li text="Shorts" icon={icons.shortIcon}/>
-                        <Li text="Subscriptions" icon={icons.subscriptionsIcon}/>
-                        <Li text="Originals" icon={icons.originalsIcon}/>
-                        <Li text="YouTube Music" icon={icons.musicIcon}/>
-                        <hr className={styles.section_separator}/>
-
-                        <Li text="Library" icon={icons.libraryIcon}/>
-                        <Li text="History" icon={icons.historyIcon}/>
-                        <Li text="Your Videos" icon={icons.videosIcon}/>
-                        <Li text="Watch later" icon={icons.clockIcon}/>
-                        <Li text="Download" icon={icons.downloadIcon}/>
-                        <Li text="Liked videos" icon={icons.likedIcon}/>
-                        <Li text="Show more" icon={icons.arrowDownIcon}/>
+                        {navigation.map(nav => (
+                        <Li text={nav.text} icon={nav.icon}/>
+                        ))}
 
                         <hr className={styles.section_separator}/>
+
+                        {navigationTwo.map(nav => (
+                        <Li text={nav.text} icon={nav.icon}/>
+
+                        ))}
+
+                        <hr className={styles.section_separator}/>
+
                         <Li text="SUBSCRIPTIONS" type={'text'}/>
-                                {subscriptions.map(subs => (
-                                <LiSubs text={subs.name} img={subs.img}/>
-                                ))}
+                        {subscriptions.map(subs => (
+                        <LiSubs text={subs.name} img={subs.img}/>
+                        ))}
                 
-
+                
                         <hr className={styles.section_separator}/>
                         <Li text="EXPLORE" type={'text'}/>
-                        <Li text="Movies" icon={icons.moviesIcon}/>
-                        <Li text="Gaming" icon={icons.gamingIcon}/>
-                        <Li text="Live" icon={icons.liveIcon}/>
-                        <Li text="Learning" icon={icons.lightIcon}/>
-                        <Li text="Sports" icon={icons.trophyIcon}/>
-                        <hr className={styles.section_separator}/>
-                        <Li text="Settings" icon={icons.settingsIcon}/>
-                        <Li text="Report history" icon={icons.flagIcon}/>
-                        <Li text="Help" icon={icons.helpIcon}/>
-                        <Li text="Send feedback" icon={icons.exclamationIcon}/>
-                        <hr className={styles.section_separator}/>
 
+                        {navExplore.map(nav => (
+                        <Li text={nav.text} icon={nav.icon}/>
+
+                        ))}
+                        
+                        <hr className={styles.section_separator}/>
+                        
+                        {navSettings.map(nav => (
+                        <Li text={nav.text} icon={nav.icon}/>
+
+                        ))}
+                        <p>
+                        AboutPressCopyrightContact usCreatorsAdvertiseDevelopers
+                        TermsPrivacyPolicy & SafetyHow YouTube worksTest new features
+                        © 2022 Google LLC
+                        </p>
+
+                        <hr className={styles.section_separator}/>
                 </div>
                 </ul>
         </div>
 
         <div className={styles.container_card}>
-
                 {children}
-                
         </div>
-
 </div>
 
 
